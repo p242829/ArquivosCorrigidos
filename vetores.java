da9ac5aba3d6d0a71acb4edf98dd814350394357
p242829 <p242829@dac.unicamp.br>
2023-04-05 20:45:02 -0300

    hoje

---
+++ vetores.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class vetores {
+    Scanner sc = new Scanner(System.in);
+    public void exemplo1(){
+        //criando um vetor
+        double[] valores = new double[]{10.5,20.7,30.6,40.4};
+        System.out.println("1º Posição - Índice[0]"+ valores[0]);
+        System.out.println("2º Posição - Índice[1]"+ valores[1]);
+        System.out.println("3º Posição - Índice[2]"+ valores[2]);
+        System.out.println("4º Posição - Índice[3]"+ valores[3]);
+        valores[0] = //
+    }
+    public void exemplo2(){
+        //criando um vetor
+        int valoresInt[] = new int[5];
+        System.out.println("digite 5 número inteiros");
+        valoresInt[0]=sc.nextInt();
+        valoresInt[1]=sc.nextInt();
+        valoresInt[2]=sc.nextInt();
+        valoresInt[3]=sc.nextInt();
+        valoresInt[4]=sc.nextInt();
+        System.out.println("1º Posição - Índice[0]"+ valoresInt);
+        System.out.println("2º Posição - Índice[1]"+ valoresInt);
+        System.out.println("3º Posição - Índice[2]"+ valoresInt);
+        System.out.println("4º Posição - Índice[3]"+ valoresInt);
+        
+
+
+    }
+    
+}
